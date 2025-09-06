fc5fc93a5b7210a9579f162179bffb684a9f4679
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-09-06 23:02:31 +0400

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,12 +15,15 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	size_t j;
 	int temp;
+	int swapped;
 
 	if (array  == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
+
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -28,10 +31,13 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				
+				swapped = 1;
 				print_array(array, size);
 			}
 
 		}
+
+		if (swapped == 0)
+			break;
 	}
 }
