9b81746c790e8cebcd26b65fc62a8e6408ae95c6
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-09-06 22:45:39 +0400

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array in ascending order using the bubble sort
+ * @array: expected to be printed when we swap two elements
+ * @size: size of the array
+ *
+ * Return: big o notation of the time complexity
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	int temp;
+
+	if (array  == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				
+				print_array(array, size);
+			}
+
+		}
+	}
+}
